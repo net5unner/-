d8adc56d823eaa642e04f30a2f6b5227960c2747
NetRunner <alex283rus@gmail.com>
2023-02-03 23:33:03 +0300

    создали и решили задачу 4

---
+++ задача 4/Program.cs
@@ -0,0 +1,17 @@
+﻿// Задача 4: Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел.
+
+Console.Clear();
+
+Console.WriteLine("Введите число :x ");
+int x = int.Parse(Console.ReadLine()!);
+
+Console.WriteLine("Введите число :y ");
+int y = int.Parse(Console.ReadLine()!);
+
+Console.WriteLine("Введите число :z ");
+int z = int.Parse(Console.ReadLine()!);
+
+if (x > y && x > z) Console.WriteLine($" максимальное число {x}");
+if (y > x && y > z) Console.WriteLine($" максимальное число {y}");
+if (z > x && z > y) Console.WriteLine($" максимальное число {z}");
+if (x == y && z == y && x == z) Console.WriteLine("числа равны друг другу");
