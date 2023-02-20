78e7c75cd09dc7160dc5a23407e97bf197772ba7
NetRunner <alex283rus@gmail.com>
2023-02-20 11:15:40 +0300

    решили задачу из непроверяемого

---
+++ Задача_А/Program.cs
@@ -0,0 +1,15 @@
+﻿// Задача_а_Подсчитать сумму цифр в числе.
+
+Console.Clear();
+
+Console.WriteLine("Введите любое число: ");
+
+int num = int.Parse(Console.ReadLine()!);
+
+int sum =0;
+while (num > 0)
+{
+sum = sum + num % 10;
+num /= 10;
+}
+Console.WriteLine($"Сумма цифр числа равна {sum}");
