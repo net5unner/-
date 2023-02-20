79252cc06dba1cc8e6e4b351a647543506920634
NetRunner <alex283rus@gmail.com>
2023-02-20 11:23:48 +0300

    решили задачу 36

---
+++ ЗАДАЧА_36/Program.cs
@@ -0,0 +1,30 @@
+﻿// Задача 36: Задайте одномерный массив, заполненный случайными числами. 
+// Найдите сумму элементов, стоящих на нечётных позициях.
+
+Console.Clear();
+
+int[] GetArray(int size, int minValue, int maxValue)
+{
+    int[] result = new int[size];
+    for (int i = 0; i < size; i++)
+    {
+        result[i] = new Random().Next(minValue, maxValue + 1);
+    }
+    return result;
+}
+int Result(int[] inArray)
+{
+    int count = 0;
+    for (int i = 0; i < inArray.Length; i++)
+    {
+        if (i % 2 !=0)
+        {
+        count += inArray[i];
+        }
+    }    
+return count;
+}
+int[] array = GetArray(9, -15, 84);
+Console.WriteLine(String.Join(", ", array));
+int count = Result(array);
+Console.WriteLine(count);
