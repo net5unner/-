02264d2ca125058f139bf516e8d65f69a9f98ab5
NetRunner <alex283rus@gmail.com>
2023-02-27 14:13:52 +0300

    Сделал 54 задачу

---
+++ Задача 54/Program.cs
@@ -0,0 +1,60 @@
+﻿// Задайте двумерный массив. Напишите программу, 
+// которая упорядочит по убыванию элементы каждой строки массива.
+
+Console.Clear();
+
+int[,] GetArray(int m, int n, int minValue, int maxValue)
+
+{
+    int[,] result = new int[m, n];
+
+    for (int i = 0; i < m; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            result[i, j] = new Random().Next(minValue, maxValue + 1);
+        }
+    }
+    return result;
+}
+void PrintArray(int[,] inArray)
+{
+    for (int i = 0; i < inArray.GetLength(0); i++)
+    {
+        for (int j = 0; j < inArray.GetLength(1); j++)
+        {
+            Console.Write($"{inArray[i, j]}\t ");
+        }
+        Console.WriteLine();
+    }
+}
+void PrintMaxToMin(int[,] inArray)
+{
+    for (int i = 0; i < inArray.GetLength(0); i++)
+    {
+        for (int j = 0; j < inArray.GetLength(1); j++)
+        {
+            for (int k = 0; k < inArray.GetLength(1) - 1; k++)
+            {
+                if  (inArray[i, k] < inArray[i,k + 1])
+                {
+                    int temp = inArray[i, k +1];
+                    inArray[i, k + 1] = inArray[i, k];
+                    inArray[i, k] = temp;
+                }
+            }
+        }
+    }
+}
+
+Console.Write("Введите количество строк: ");
+int str = int.Parse(Console.ReadLine()!);
+Console.Write("Введите количество столбцов: ");
+int col = int.Parse(Console.ReadLine()!);
+
+int[,] array = GetArray(str, col, -9, 14);
+
+PrintArray(array);
+Console.WriteLine();
+PrintMaxToMin(array);
+PrintArray(array);
