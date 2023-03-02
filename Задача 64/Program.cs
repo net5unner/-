fa2cf2d91e71a208680ca272a697e138547483bf
NetRunner <alex283rus@gmail.com>
2023-03-02 18:52:24 +0300

    решил задачу 64

---
+++ Задача 64/Program.cs
@@ -0,0 +1,20 @@
+﻿// Задача 64: Задайте значения N. Напишите программу, которая выведет все натуральные числа в промежутке от N до 1.
+
+Console.Write("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+int count = 1;
+NaturalToLow(number, count);
+
+
+void NaturalToLow(int n, int count)
+{
+    if (count > n)
+    {
+        return;
+    }
+    else
+    {
+        NaturalToLow(n, count + 1);
+        Console.Write(count + " ");
+    }
+}
